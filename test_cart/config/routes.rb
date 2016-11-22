e02d300f6cbd0da9c950b0ed1d913fa5f648e088
sathi <rowshan_sathi@yahoo.com>
2016-11-22 21:29:26 +0100

    added views

--- test_cart/config/routes.rb
+++ test_cart/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'carts#index'
+  resources :carts
+
 end
