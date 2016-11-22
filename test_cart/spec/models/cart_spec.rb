7f7547b1b11e4cd436f1507a3d95205fbc73c8ff
sathi <rowshan_sathi@yahoo.com>
2016-11-22 19:14:55 +0100

    Successfull Model test added

--- test_cart/spec/models/cart_spec.rb
+++ test_cart/spec/models/cart_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Cart, type: :model do
 
   describe 'factory' do
     it 'exists' do
-      expect { FactoryGirl.build :cart }.not_to raise_error
+      expect { FactoryGirl.create :cart }.not_to raise_error
     end
 
     it 'is valid' do
@@ -12,17 +12,17 @@ RSpec.describe Cart, type: :model do
     end
 
     it 'has the attributes' do
-      expect(attributes_for(:cart)).to include(:product_name, :category)
+      expect(attributes_for(:cart)).to include(:product_name,:category)
     end
   end
 
   describe "validation of" do
 
-    context 'product' do
+    context 'product_name' do
       subject { build(:cart, product_name: nil) }
       it " fails for missing" do
         expect(subject).not_to be_valid
-        expect(subject.errors).to include(:cart)
+        expect(subject.errors).to include(:product_name)
       end
     end
 
